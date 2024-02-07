65cdfb18f9680c8f2a4689e540b13f74ed8ea184
Mouhcine Essafi <messafi1337@gmail.com>
2024-02-07 16:05:51 +0100

    edit structure for auth

--- Newsletter/routes/web.php
+++ Newsletter/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +16,3 @@ use App\Http\Controllers\Auth\RegisterController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register', [RegisterController::class, 'store']);
