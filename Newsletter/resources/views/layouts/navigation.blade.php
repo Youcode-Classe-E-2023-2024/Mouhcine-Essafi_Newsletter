d07fdaa2502199fc390a8a7a055c59219ebe0092
Mouhcine Essafi <messafi1337@gmail.com>
2024-02-15 11:05:55 +0100

    Update navigation.blade.php

--- Newsletter/resources/views/layouts/navigation.blade.php
+++ Newsletter/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,16 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('manage-roles')" :active="request()->routeIs('manage-roles')">
+                        {{ __('Role') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('media')" :active="request()->routeIs('media')">
+                        {{ __('Media') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
